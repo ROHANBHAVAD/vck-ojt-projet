bfefe14c14eaa274cd3fb1a059f254dfe8a8b9d0
ROHANBHAVAD <rohanece1888@gmail.com>
2025-06-26 12:49:35 +0530

    Rohan second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import { Route ,BrowserRouter as Router ,Routes } from "react-router-dom";
-import HomePage from "./pages/homePage";
-import AboutPage from "./pages/aboutPage";
-import CoursesPage from "./pages/coursesPage";
-import ContactPage from "./pages/contactPage";
-import './styles/pages.css'
-import AdmissionPage from "./pages/admissionPage";
-const App =() =>
-{
-  return(
-    <div>
-      <Router>
-        <Routes>
-           <Route path="/" element={<HomePage/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/about" element={<AboutPage/>}/>
-          <Route path="/courses" element={<CoursesPage/>}/>
-          <Route path="/contact" element={<ContactPage/>}/>
-          <Route path="/admission" element={<AdmissionPage/>}/>   
-         </Routes>
-      </Router>
-    </div>
-  )
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Headers/Header";
+import Footer from "./components/Footer/Footer";
+import ContactPage from "./pages/ContactPage";
+import "./styles/pages.css";
+import AboutPage from "./pages/AboutPage";
+import CoursesPage from "./pages/CoursesPage";
+import AdmissionsPage from "./pages/AdmissionsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="main-layout">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/admissions" element={<AdmissionsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+       
+      </div>
+       <Footer/>
+    </Router>
+    
+  );
 }
 export default App;
+
